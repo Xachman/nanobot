0f4f838ea64aaffb6adf146da5588774b267f8cb
xachman <zironside@hotmail.com>
2026-03-29 15:28:32 +0000

    add ollama_provider as active provider and general providers tests

--- nanobot/providers/__init__.py
+++ nanobot/providers/__init__.py
@@ -14,6 +14,7 @@ __all__ = [
     "OpenAICompatProvider",
     "OpenAICodexProvider",
     "AzureOpenAIProvider",
+    "OllamaProvider",
 ]
 
 _LAZY_IMPORTS = {
@@ -21,11 +22,13 @@ _LAZY_IMPORTS = {
     "OpenAICompatProvider": ".openai_compat_provider",
     "OpenAICodexProvider": ".openai_codex_provider",
     "AzureOpenAIProvider": ".azure_openai_provider",
+    "OllamaProvider": ".ollama_provider",
 }
 
 if TYPE_CHECKING:
     from nanobot.providers.anthropic_provider import AnthropicProvider
     from nanobot.providers.azure_openai_provider import AzureOpenAIProvider
+    from nanobot.providers.ollama_provider import OllamaProvider
     from nanobot.providers.openai_compat_provider import OpenAICompatProvider
     from nanobot.providers.openai_codex_provider import OpenAICodexProvider
 
